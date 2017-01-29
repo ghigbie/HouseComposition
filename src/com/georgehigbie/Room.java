68810502351ec3afa5bd84b9d7c97a026dff14f0
George Higbie <gehigbie@gmail.com>
2017-01-28 19:45:24 -0700

    added getters and setters

--- src/com/georgehigbie/Room.java
+++ src/com/georgehigbie/Room.java
@@ -16,4 +16,37 @@ public class Room {
         this.numberDoors = numberDoors;
         this.roomName = roomName;
     }
+
+    //auto generated getters and setters
+    public int getNumberWalls() {
+        return numberWalls;
+    }
+
+    public void setNumberWalls(int numberWalls) {
+        this.numberWalls = numberWalls;
+    }
+
+    public int getNumberWindows() {
+        return numberWindows;
+    }
+
+    public void setNumberWindows(int numberWindows) {
+        this.numberWindows = numberWindows;
+    }
+
+    public int getNumberDoors() {
+        return numberDoors;
+    }
+
+    public void setNumberDoors(int numberDoors) {
+        this.numberDoors = numberDoors;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
 }
