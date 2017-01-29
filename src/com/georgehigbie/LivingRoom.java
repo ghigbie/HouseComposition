5dad4579218afcfef97e984389dc5f495bc8aa28
George Higbie <gehigbie@gmail.com>
2017-01-28 19:47:03 -0700

    added sofa class and living room class

---
+++ src/com/georgehigbie/LivingRoom.java
@@ -0,0 +1,14 @@
+package com.georgehigbie;
+
+/**
+ * Created by georgehigbie on 1/28/17.
+ */
+public class LivingRoom extends Room {
+
+
+    public LivingRoom(int numberWalls, int numberWindows, int numberDoors, String roomName) {
+        super(numberWalls, numberWindows, numberDoors, roomName);
+    }
+
+
+}
