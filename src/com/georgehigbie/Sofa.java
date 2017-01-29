7c58ef4a7aec4aa476cd4131249066efa9c0cc60
George Higbie <gehigbie@gmail.com>
2017-01-28 19:48:24 -0700

    added getters and setters

--- src/com/georgehigbie/Sofa.java
+++ src/com/georgehigbie/Sofa.java
@@ -4,4 +4,29 @@ package com.georgehigbie;
  * Created by georgehigbie on 1/28/17.
  */
 public class Sofa {
+
+    private int numberSeats;
+    private String plushness;
+
+    public Sofa(int numberSeats, String plushness) {
+        this.numberSeats = numberSeats;
+        this.plushness = plushness;
+    }
+
+    //auto generated getters and setters
+    public int getNumberSeats() {
+        return numberSeats;
+    }
+
+    public void setNumberSeats(int numberSeats) {
+        this.numberSeats = numberSeats;
+    }
+
+    public String getPlushness() {
+        return plushness;
+    }
+
+    public void setPlushness(String plushness) {
+        this.plushness = plushness;
+    }
 }
