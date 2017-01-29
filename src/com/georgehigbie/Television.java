175dd329d11bb46804856e2a2b860cbed81cab43
George Higbie <gehigbie@gmail.com>
2017-01-28 19:56:00 -0700

    added TV class

---
+++ src/com/georgehigbie/Television.java
@@ -0,0 +1,43 @@
+package com.georgehigbie;
+
+/**
+ * Created by georgehigbie on 1/28/17.
+ */
+public class Television {
+
+    private int size;
+    private boolean hasInternet;
+
+    public Television(int size, boolean hasInternet) {
+        this.size = size;
+        this.hasInternet = hasInternet;
+    }
+
+    public void turnOn(){
+        System.out.println("The TV is now on, and people are watching.");
+    }
+
+    public void getNetflix(){
+        if(hasInternet == true){
+            System.out.println("The TV is now showing Netflix.");
+        }else{
+            System.out.println("Please buy a TV with internet, you cheapskate.");
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public boolean isHasInternet() {
+        return hasInternet;
+    }
+
+    public void setHasInternet(boolean hasInternet) {
+        this.hasInternet = hasInternet;
+    }
+}
